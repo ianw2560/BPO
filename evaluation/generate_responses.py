ef821f55a3c8b67f5e868c65b5c4b62ee7e56eed
Ian Wallace <ian.wallace2560@gmail.com>
2024-11-24 21:30:33 -0500

    add missing formatting

--- evaluation/generate_responses.py
+++ evaluation/generate_responses.py
@@ -60,7 +60,7 @@ def generate_optimized_prompt_bpo(prompt: str, device, tokenizer, model):
 
     model_inputs = tokenizer(prompt, return_tensors="pt").to(device)
     output = model.generate(**model_inputs, max_new_tokens=1024, do_sample=True, top_p=0.9, temperature=0.6, num_beams=1)
-    optimized_prompt = tokenizer.decode(output[0], skip_special_tokens=True)
+    optimized_prompt = tokenizer.decode(output[0], skip_special_tokens=True).split('[/INST]')[1].strip()
 
     return optimized_prompt
 
