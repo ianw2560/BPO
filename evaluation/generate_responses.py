54b2c2d9bed46664e5e2de30594311b32f29a58a
Ian Wallace <ian.wallace2560@gmail.com>
2024-11-24 22:06:12 -0500

    Split

--- evaluation/generate_responses.py
+++ evaluation/generate_responses.py
@@ -60,7 +60,7 @@ def generate_optimized_prompt_bpo(prompt: str, device, tokenizer, model):
 
     model_inputs = tokenizer(prompt, return_tensors="pt").to(device)
     output = model.generate(**model_inputs, max_new_tokens=1024, do_sample=True, top_p=0.9, temperature=0.6, num_beams=1)
-    optimized_prompt = tokenizer.decode(output[0], skip_special_tokens=True).split('[/INST]')[1].strip().split("\"").strip()
+    optimized_prompt = tokenizer.decode(output[0], skip_special_tokens=True).split('[/INST]')[1].strip().split("\"")[1].strip()
 
     print(optimized_prompt)
 
