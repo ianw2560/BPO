e978b4ae5b34c14f1303ac38fb64f99bb9d7f990
Ian Wallace <ian.wallace2560@gmail.com>
2024-12-01 16:12:40 -0500

    Update temp

--- evaluation/generate_responses.py
+++ evaluation/generate_responses.py
@@ -67,7 +67,7 @@ def generate_optimized_prompt_bpo(prompt: str, context: str, device, tokenizer, 
     # print(prompt)
 
     model_inputs = tokenizer(prompt, return_tensors="pt").to(device)
-    output = model.generate(**model_inputs, max_new_tokens=1024, do_sample=True, top_p=0.9, temperature=0.3, num_beams=1)
+    output = model.generate(**model_inputs, max_new_tokens=1024, do_sample=True, top_p=0.9, temperature=0.6, num_beams=1)
     optimized_prompt = tokenizer.decode(output[0], skip_special_tokens=True).split('[/INST]')[1].strip()
 
     print("RAW RESPONSE")
